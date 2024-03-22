14d9422643c0e4d31721755b665d27467d98363c
Mahboob-A <iammahboob.a@gmail.com>
2024-03-22 22:23:12 +0530

    Add: intro to file handler

---
+++ context-manager/sample-dir-change.py
@@ -0,0 +1,18 @@
+import os 
+
+
+def to_dummy_dir_1(): 
+        main_cwd = os.getcwd()
+        print()
+        print('parent dir: ', main_cwd)
+        
+        os.chdir('dummy-dir-1')
+        cwd = os.getcwd()
+        print('after changing cwd: ', cwd)
+        
+        os.chdir(main_cwd)
+        cwd = os.getcwd()
+        print('after dir change back to parent: ', cwd)
+        
+
+to_dummy_dir_1()
